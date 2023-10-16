1651c4b4773a2e087af0b98eb6dcf8b529996408
Fiona-Nyadero <fionanyadero@gmail.com>
2023-10-16 00:13:11 +0000

    Task 6

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """ Defines the HBNBCommand class """
+    
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exits the program (Ctrl-D)"""
+        print("")
+        return True
+
+    def emptyline(self):
+        """ No command given """
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
